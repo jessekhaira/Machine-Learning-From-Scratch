47b124e42eb55d9c1470c25a989f91739cbd37a7
13jk59 <jesse.khaira10@gmail.com>
2021-04-27 11:35:32 -0600

    changed names of modules to conform to snake casing

--- ML_algorithms/Supervised_Learning/Regression/Tests/test_regression_tree.py
+++ ML_algorithms/Supervised_Learning/Regression/Tests/test_regression_tree.py
@@ -1,9 +1,9 @@
 from ML_algorithms.Supervised_Learning.Regression.RegressionTree import RegressionTree
 import unittest
-import numpy as np 
+import numpy as np
 import sklearn
 from sklearn.datasets import load_boston
-from ML_algorithms.Utility.ScoreFunctions import RMSE,MSE,MAE,TSS,RSS
+from ML_algorithms.Utility.ScoreFunctions import RMSE, MSE, MAE, TSS, RSS
 from ML_algorithms.Utility.k_Fold_CV import k_fold_CV
 import unittest
 
@@ -14,11 +14,12 @@ kCV = k_fold_CV()
 
 
 class tests(unittest.TestCase):
+
     def testFit(self):
         # The tree is not being constrained when it is fitting
-        # thus we should be able to achieve 0 error AKA all residuals are 0 
+        # thus we should be able to achieve 0 error AKA all residuals are 0
         regressionObj = RegressionTree(minSamplesSplit=1)
-        regressionObj.fit(X1,Y1)
+        regressionObj.fit(X1, Y1)
         predictions = regressionObj.predict(X1)
         rmse = RMSE(Y1, predictions)
         mae = MAE(Y1, predictions)
@@ -40,26 +41,34 @@ class tests(unittest.TestCase):
 
     def test_sanityChecks(self):
         # If we regularize the tree, we should get a higher error than if we don't
-        regressionObj2 = RegressionTree(minSamplesSplit=5, maxDepth=3, min_impurity_decrease=0.15)
-        regressionObj2.fit(X1,Y1)
+        regressionObj2 = RegressionTree(minSamplesSplit=5,
+                                        maxDepth=3,
+                                        min_impurity_decrease=0.15)
+        regressionObj2.fit(X1, Y1)
         predictions2 = regressionObj2.predict(X1)
         error2 = RMSE(Y1, predictions2)
 
-        # Sanity checks - regularization is so high all we get is one leaf 
+        # Sanity checks - regularization is so high all we get is one leaf
         # meaning all predictions are equal to mean of labels
         # meaning that the RSS of the predictions should be equal to TSS of the label
-        regressionObj3 = RegressionTree(minSamplesSplit=10, maxDepth=0, min_impurity_decrease=0.15)
-        regressionObj3.fit(X1,Y1)
+        regressionObj3 = RegressionTree(minSamplesSplit=10,
+                                        maxDepth=0,
+                                        min_impurity_decrease=0.15)
+        regressionObj3.fit(X1, Y1)
         predictions3 = regressionObj3.predict(X1)
         error3 = RMSE(Y1, predictions3)
 
-        regressionObj4 = RegressionTree( minSamplesSplit=1000, maxDepth=10, min_impurity_decrease=0.15)
-        regressionObj4.fit(X1,Y1)
+        regressionObj4 = RegressionTree(minSamplesSplit=1000,
+                                        maxDepth=10,
+                                        min_impurity_decrease=0.15)
+        regressionObj4.fit(X1, Y1)
         predictions4 = regressionObj4.predict(X1)
         error4 = RMSE(Y1, predictions4)
 
-        regressionObj5 = RegressionTree( minSamplesSplit=10, maxDepth=10, min_impurity_decrease=1)
-        regressionObj5.fit(X1,Y1)
+        regressionObj5 = RegressionTree(minSamplesSplit=10,
+                                        maxDepth=10,
+                                        min_impurity_decrease=1)
+        regressionObj5.fit(X1, Y1)
         predictions5 = regressionObj4.predict(X1)
         error5 = RMSE(Y1, predictions5)
 
@@ -67,6 +76,7 @@ class tests(unittest.TestCase):
         self.assertAlmostEqual(RSS(Y1, predictions3), TSS(Y1))
         self.assertAlmostEqual(RSS(Y1, predictions4), TSS(Y1))
         self.assertAlmostEqual(RSS(Y1, predictions5), TSS(Y1))
+
 
 if __name__ == "__main__":
     unittest.main()
