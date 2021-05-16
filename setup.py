870258bf3c1c9d8c9d541d7fc069c5a1fc094fc4
13jk59 <jesse.khaira10@gmail.com>
2021-05-16 12:57:26 -0600

    localized all tests + cleaned up setup.py

--- setup.py
+++ setup.py
@@ -2,26 +2,26 @@ import setuptools
 from codecs import open
 from os import path
 
-__version__ = '0.0.1'
+__version__ = "0.0.1"
 
 here = path.abspath(path.dirname(__file__))
 
 # get the dependencies and installs
-with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
     all_reqs = f.read().split('\n')
 
-requirements_installation= [x.strip() for x in all_reqs if 'git+' not in x]
+requirements_installation = [x.strip() for x in all_reqs if 'git+' not in x]
 
 requirements_installation.pop()
 
 setuptools.setup(
-    name="MachineLearning_Scratch", 
-    version= __version__,
+    name="MachineLearning_Scratch",
+    version=__version__,
     author="Jesse Khaira",
     author_email="jesse.khaira15@gmail.com",
-    license = 'MIT',
-    description="Python implementations using only NumPy of some foundational ML algorithms",
+    license="MIT",
+    description=
+    "Python implementations using only NumPy of some foundational ML algorithms",
     url="https://github.com/13jk59/MachineLearning_Scratch.git",
     packages=setuptools.find_packages(),
-    install_requires = requirements_installation
-)
+    install_requires=requirements_installation)
