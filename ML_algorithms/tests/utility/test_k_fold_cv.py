7c04dde2d7f254ea2061f9e0e80bdae6ca8f40d7
13jk59 <jesse.khaira10@gmail.com>
2021-05-17 14:40:59 -0600

    wrapped kfold cv test in unittest

--- ML_algorithms/tests/utility/test_k_fold_cv.py
+++ ML_algorithms/tests/utility/test_k_fold_cv.py
@@ -4,14 +4,24 @@ from sklearn.datasets import load_breast_cancer
 from ML_algorithms.Supervised_Learning.Classifiers.Logistic_Regression import LogisticRegression
 from ML_algorithms.Utility.ScoreFunctions import accuracy
 from ML_algorithms.Utility.k_Fold_CV import k_fold_CV
-##-- MANUAL TEST W/ Step through debugging----
-X, Y = load_breast_cancer(return_X_y=True)
+import unittest
 
-X = preprocessing.scale(X).T
-Y = Y.T.reshape(1, -1)
 
-LR1 = LogisticRegression(X.shape[0], classificationThreshold=0.5)
+class TestKFoldCV(unittest.TestCase):
+    """ This class contains unit tests testing the K Fold CV
+    algorithm """
 
-output = k_fold_CV().getKScore(X, Y, accuracy, LR1)
+    def test_1(self):
+        X, Y = load_breast_cancer(return_X_y=True)
+        X = preprocessing.scale(X).T
+        Y = Y.T.reshape(1, -1)
 
-print(output)    ## Seems reasonable
+        LR1 = LogisticRegression(X.shape[0], classificationThreshold=0.5)
+
+        output = k_fold_CV().getKScore(X, Y, accuracy, LR1)
+
+        self.assertGreaterEqual(output, 0.95)
+
+
+if __name__ == "__main__":
+    unittest.main()
