60549e44064114c4b20807aaccb560b350c44448
13jk59 <jesse.khaira10@gmail.com>
2021-05-28 14:15:30 -0600

    fixed naming

--- ML_algorithms/tests/supervised_learning/classifiers/test_classification_tree.py
+++ ML_algorithms/tests/supervised_learning/classifiers/test_classification_tree.py
@@ -27,69 +27,71 @@ class TestsClassificationTree(unittest.TestCase):
         self.y2 = None
         self.k_cv = None
 
-    def testMultiClass(self):
+    def test_multi_class(self):
         # Should be able to overfit multiiclasses easy
         # notice lack of preprocessing - no need to normalize features
         # no need to one hot encode labels
 
         # out of box model :D
-        classificationObj = ClassificationTree(entropy=False, minSamplesSplit=1)
-        classificationObj.fit(self.x2, self.y2)
-        predictions = classificationObj.predict(self.x2)
+        classification_obj = ClassificationTree(entropy=False,
+                                                minSamplesSplit=1)
+        classification_obj.fit(self.x2, self.y2)
+        predictions = classification_obj.predict(self.x2)
         acc = accuracy(self.y2, predictions)
         self.assertEqual(acc, 1)
 
-        classificationObj2 = ClassificationTree(entropy=False,
-                                                minSamplesSplit=1)
+        classification_obj2 = ClassificationTree(entropy=False,
+                                                 minSamplesSplit=1)
         kScore = self.k_cv.getKScore(self.x2, self.y2, accuracy,
-                                     classificationObj2)
+                                     classification_obj2)
         self.assertEqual(kScore, 1)
 
-    def testBinary(self):
+    def test_binary(self):
         # The tree we are growing is unconstrained so it should be able to
         # fit the training set perfectly 100% (AKA overfitting  :) )
-        classificationObj = ClassificationTree(entropy=False, minSamplesSplit=1)
-        classificationObj.fit(self.x1, self.y1)
-        predictions = classificationObj.predict(self.x1)
+        classification_obj = ClassificationTree(entropy=False,
+                                                minSamplesSplit=1)
+        classification_obj.fit(self.x1, self.y1)
+        predictions = classification_obj.predict(self.x1)
         acc = accuracy(self.y1, predictions)
         self.assertEqual(acc, 1)
-        classificationObj2 = ClassificationTree(entropy=False,
-                                                minSamplesSplit=1)
+        classification_obj2 = ClassificationTree(entropy=False,
+                                                 minSamplesSplit=1)
         kScore = self.k_cv.getKScore(self.x1, self.y1, accuracy,
-                                     classificationObj2)
+                                     classification_obj2)
         self.assertEqual(kScore, 1)
 
-    def test_sanityChecks(self):
-        classificationObj2 = ClassificationTree(entropy=False,
-                                                minSamplesSplit=5,
-                                                maxDepth=3,
-                                                min_impurity_decrease=0.09)
-        classificationObj2.fit(self.x1, self.y1)
-        predictions2 = classificationObj2.predict(self.x1)
+    def test_sanity_checks(self):
+        classification_obj2 = ClassificationTree(entropy=False,
+                                                 minSamplesSplit=5,
+                                                 maxDepth=3,
+                                                 min_impurity_decrease=0.09)
+        classification_obj2.fit(self.x1, self.y1)
+        predictions2 = classification_obj2.predict(self.x1)
         acc2 = accuracy(self.y1, predictions2)
 
-        classificationObj3 = ClassificationTree(entropy=False,
-                                                minSamplesSplit=10,
-                                                maxDepth=0,
-                                                min_impurity_decrease=0.15)
-        classificationObj3.fit(self.x1, self.y1)
-        predictions3 = classificationObj3.predict(self.x1)
+        classification_obj3 = ClassificationTree(entropy=False,
+                                                 minSamplesSplit=10,
+                                                 maxDepth=0,
+                                                 min_impurity_decrease=0.15)
+        classification_obj3.fit(self.x1, self.y1)
+        predictions3 = classification_obj3.predict(self.x1)
         acc3 = accuracy(self.y1, predictions3)
 
-        classificationObj4 = ClassificationTree(entropy=False,
-                                                minSamplesSplit=1000,
-                                                maxDepth=10,
-                                                min_impurity_decrease=0.15)
-        classificationObj4.fit(self.x1, self.y1)
-        predictions4 = classificationObj4.predict(self.x1)
+        classification_obj4 = ClassificationTree(entropy=False,
+                                                 minSamplesSplit=1000,
+                                                 maxDepth=10,
+                                                 min_impurity_decrease=0.15)
+        classification_obj4.fit(self.x1, self.y1)
+        predictions4 = classification_obj4.predict(self.x1)
         acc4 = accuracy(self.y1, predictions4)
 
-        classificationObj5 = ClassificationTree(entropy=False,
-                                                minSamplesSplit=10,
-                                                maxDepth=10,
-                                                min_impurity_decrease=1)
-        classificationObj5.fit(self.x1, self.y1)
-        predictions5 = classificationObj4.predict(self.x1)
+        classification_obj5 = ClassificationTree(entropy=False,
+                                                 minSamplesSplit=10,
+                                                 maxDepth=10,
+                                                 min_impurity_decrease=1)
+        classification_obj5.fit(self.x1, self.y1)
+        predictions5 = classification_obj4.predict(self.x1)
         acc5 = accuracy(self.y1, predictions5)
 
         self.assertLessEqual(acc2, 1)
