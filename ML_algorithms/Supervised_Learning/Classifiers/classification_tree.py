7418d3ae4cde439febacfe065b476df81beb7fa1
13jk59 <jesse.khaira10@gmail.com>
2021-05-28 14:21:26 -0600

    cleaned up the classification tree algorithm module

--- ML_algorithms/Supervised_Learning/Classifiers/classification_tree.py
+++ ML_algorithms/Supervised_Learning/Classifiers/classification_tree.py
@@ -1,34 +1,44 @@
+""" This module contains code representing the classification decision
+tree algorithm """
 from ML_algorithms.Supervised_Learning.Base_Classes.DecisionTree import BaseDecisionTree
 from ML_algorithms.Utility.DecisionTreeFunctions import entropyGain
 from ML_algorithms.Utility.DecisionTreeFunctions import giniGain
 from ML_algorithms.Utility.DecisionTreeFunctions import predictionClassification
+from typing import Union
 
 
 class ClassificationTree(BaseDecisionTree):
-    """
-    This class represents a classification decision tree trained using either entropy based information gain or
-    gini based information gain.
+    """ This class implements the BaseDecisionTree class for
+    the purposes of performing classification.
 
-    Parameters:
-    -> entropy (boolean): Boolean value indicating whether to train using entropy based information gain. 
+    Attributes:
+        entropy:
+            Boolean value indicating whether to train using entropy based
+            information gain.
 
-    -> gini (boolean): Boolean value indicating whether to train using gini based information gain. 
+        minSamplesSplit:
+            Integer indicating the minimum number of examples that have
+            to fall in this node to justify splitting further
 
-    -> minSamplesSplit (int): Integer indicating the minimum number of examples that have to fall in this node to justify splitting further
+        maxDepth:
+            Integer representing the maximum depth to grow this tree,
+            or None if not wanted
 
-    -> maxDepth (int): Integer representing the maximum depth to grow this tree 
-    
-    -> maxFeatures (int): Integer representing the maximum number of features to use to determine the split
+        maxFeatures:
+            Integer representing the maximum number of features to use
+            to determine the split, or None if not wanted
 
-    -> min_impurity_decrease (int): The minimum decrease in impurity to justify splitting a node 
+        min_impurity_decrease:
+            Integer representing the minimum decrease in impurity to justify
+            splitting a node
     """
 
     def __init__(self,
-                 entropy=True,
-                 minSamplesSplit=2,
-                 maxDepth=None,
-                 maxFeatures=None,
-                 min_impurity_decrease=0):
+                 entropy: bool = True,
+                 minSamplesSplit: int = 2,
+                 maxDepth: Union[None, int] = None,
+                 maxFeatures: Union[None, int] = None,
+                 min_impurity_decrease: int = 0):
         if entropy:
             trainFunction = entropyGain
         else:
