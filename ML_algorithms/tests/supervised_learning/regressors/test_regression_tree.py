07bba1f05a9eae3054e5eed0042d7e5c673fe41e
13jk59 <jesse.khaira10@gmail.com>
2021-05-29 13:51:08 -0600

    cleaned up testing module

--- ML_algorithms/tests/supervised_learning/regressors/test_regression_tree.py
+++ ML_algorithms/tests/supervised_learning/regressors/test_regression_tree.py
@@ -6,35 +6,42 @@ from ML_algorithms.Supervised_Learning.Regression.regression_tree import Regress
 from ML_algorithms.Utility.ScoreFunctions import RMSE, MSE, MAE, TSS, RSS
 from ML_algorithms.Utility.k_Fold_CV import k_fold_CV
 
-X1, Y1 = load_boston(return_X_y=True)
-X1 = X1.T
-Y1 = Y1.T.reshape(1, -1)
-kCV = k_fold_CV()
-
 
 class RegressionTreeTests(unittest.TestCase):
     """ This class contains test for the regression decision tree
     algorithm """
 
+    def setUp(self):
+        self.x1, self.y1 = load_boston(return_X_y=True)
+        self.x1 = self.x1.T
+        self.y1 = self.y1.T.reshape(1, -1)
+        self.k_cv = k_fold_CV()
+
+    def tearDown(self):
+        self.x1 = None
+        self.y1 = None
+        self.k_cv = None
+
     def testFit(self):
         # The tree is not being constrained when it is fitting
         # thus we should be able to achieve 0 error AKA all residuals are 0
-        regressionObj = RegressionTree(minSamplesSplit=1)
-        regressionObj.fit(X1, Y1)
-        predictions = regressionObj.predict(X1)
-        rmse = RMSE(Y1, predictions)
-        mae = MAE(Y1, predictions)
-        mse = MSE(Y1, predictions)
-        rmse = RMSE(Y1, predictions)
+        regression_obj = RegressionTree(minSamplesSplit=1)
+        regression_obj.fit(self.x1, self.y1)
+        predictions = regression_obj.predict(self.x1)
+        rmse = RMSE(self.y1, predictions)
+        mae = MAE(self.y1, predictions)
+        mse = MSE(self.y1, predictions)
+        rmse = RMSE(self.y1, predictions)
         self.assertEqual(mae, 0)
         self.assertEqual(mse, 0)
         self.assertEqual(rmse, 0)
 
         # test generalization of regression tree
-        regressionObj2 = RegressionTree(minSamplesSplit=1)
-        kScoreRMSE = kCV.getKScore(X1, Y1, RMSE, regressionObj2)
-        kScoreMSE = kCV.getKScore(X1, Y1, MSE, regressionObj2)
-        kScoreMAE = kCV.getKScore(X1, Y1, MAE, regressionObj2)
+        regression_obj2 = RegressionTree(minSamplesSplit=1)
+        kScoreRMSE = self.k_cv.getKScore(self.x1, self.y1, RMSE,
+                                         regression_obj2)
+        kScoreMSE = self.k_cv.getKScore(self.x1, self.y1, MSE, regression_obj2)
+        kScoreMAE = self.k_cv.getKScore(self.x1, self.y1, MAE, regression_obj2)
         # Dataset is easy so we should expect 0 error
         self.assertEqual(kScoreRMSE, 0)
         self.assertEqual(kScoreMSE, 0)
@@ -42,41 +49,38 @@ class RegressionTreeTests(unittest.TestCase):
 
     def test_sanityChecks(self):
         # If we regularize the tree, we should get a higher error than if we don't
-        regressionObj2 = RegressionTree(minSamplesSplit=5,
-                                        maxDepth=3,
-                                        min_impurity_decrease=0.15)
-        regressionObj2.fit(X1, Y1)
-        predictions2 = regressionObj2.predict(X1)
-        error2 = RMSE(Y1, predictions2)
+        regression_obj2 = RegressionTree(minSamplesSplit=5,
+                                         maxDepth=3,
+                                         min_impurity_decrease=0.15)
+        regression_obj2.fit(self.x1, self.y1)
+        predictions2 = regression_obj2.predict(self.x1)
+        error2 = RMSE(self.y1, predictions2)
 
         # Sanity checks - regularization is so high all we get is one leaf
         # meaning all predictions are equal to mean of labels
         # meaning that the RSS of the predictions should be equal to TSS of the label
-        regressionObj3 = RegressionTree(minSamplesSplit=10,
-                                        maxDepth=0,
-                                        min_impurity_decrease=0.15)
-        regressionObj3.fit(X1, Y1)
-        predictions3 = regressionObj3.predict(X1)
-        error3 = RMSE(Y1, predictions3)
+        regression_obj3 = RegressionTree(minSamplesSplit=10,
+                                         maxDepth=0,
+                                         min_impurity_decrease=0.15)
+        regression_obj3.fit(self.x1, self.y1)
+        predictions3 = regression_obj3.predict(self.x1)
 
-        regressionObj4 = RegressionTree(minSamplesSplit=1000,
-                                        maxDepth=10,
-                                        min_impurity_decrease=0.15)
-        regressionObj4.fit(X1, Y1)
-        predictions4 = regressionObj4.predict(X1)
-        error4 = RMSE(Y1, predictions4)
+        regression_obj4 = RegressionTree(minSamplesSplit=1000,
+                                         maxDepth=10,
+                                         min_impurity_decrease=0.15)
+        regression_obj4.fit(self.x1, self.y1)
+        predictions4 = regression_obj4.predict(self.x1)
 
-        regressionObj5 = RegressionTree(minSamplesSplit=10,
-                                        maxDepth=10,
-                                        min_impurity_decrease=1)
-        regressionObj5.fit(X1, Y1)
-        predictions5 = regressionObj4.predict(X1)
-        error5 = RMSE(Y1, predictions5)
+        regression_obj5 = RegressionTree(minSamplesSplit=10,
+                                         maxDepth=10,
+                                         min_impurity_decrease=1)
+        regression_obj5.fit(self.x1, self.y1)
+        predictions5 = regression_obj4.predict(self.x1)
 
         self.assertGreaterEqual(error2, 0)
-        self.assertAlmostEqual(RSS(Y1, predictions3), TSS(Y1))
-        self.assertAlmostEqual(RSS(Y1, predictions4), TSS(Y1))
-        self.assertAlmostEqual(RSS(Y1, predictions5), TSS(Y1))
+        self.assertAlmostEqual(RSS(self.y1, predictions3), TSS(self.y1))
+        self.assertAlmostEqual(RSS(self.y1, predictions4), TSS(self.y1))
+        self.assertAlmostEqual(RSS(self.y1, predictions5), TSS(self.y1))
 
 
 if __name__ == "__main__":
