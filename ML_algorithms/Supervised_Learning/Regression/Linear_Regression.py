330d84930a1b499ffb3aed6a5dbe0df3653c5d8d
13jk59 <jesse.khaira10@gmail.com>
2021-05-30 17:32:03 -0600

    cleaned up lin regression module

--- ML_algorithms/Supervised_Learning/Regression/Linear_Regression.py
+++ ML_algorithms/Supervised_Learning/Regression/Linear_Regression.py
@@ -1,4 +1,4 @@
-import numpy as np 
+import numpy as np
 from ML_algorithms.Neural_Net_Util.NeuralNetwork_Base import NeuralNetwork_Base
 from ML_algorithms.Neural_Net_Util.LossFunctions import mean_squared_error
 from ML_algorithms.Neural_Net_Util.ActivationFunctions import IdentityActivation
@@ -7,41 +7,57 @@ from ML_algorithms.Neural_Net_Util.Optimizers import gradientDescent
 from sklearn import preprocessing
 
 
-
-
-
 class Base_Regression(NeuralNetwork_Base):
     """
     This class represents the base class for linear regression, which all linear regression
     classes will inherit. 
     """
-    def __init__(self, degree, regularization = None, regParameter = None):
-        # Save the degree of the polynomial function that is desired to be fit. 
+
+    def __init__(self, degree, regularization=None, regParameter=None):
+        # Save the degree of the polynomial function that is desired to be fit.
         # Will be used later to transform the input features to the final function features we will fit
         self.degree = degree
-        # Loss function for regression tasks is RSS averaged over all examples = MSE 
+        # Loss function for regression tasks is RSS averaged over all examples = MSE
         lossFunction = mean_squared_error(regularization, regParameter)
-        super(Base_Regression, self).__init__(lossFunction = lossFunction, input_features = None)
+        super(Base_Regression, self).__init__(lossFunction=lossFunction,
+                                              input_features=None)
 
-    def fitGD(self, xtrain, ytrain, xvalid = None, yvalid = None, num_epochs =10, batch_size = 32, ret_train_loss = False, learn_rate = 0.01, optim = gradientDescent()):
-        # the fit method is basically the same as the neural net base, other than the transformation of the features 
-        # that needs to take place before fitting 
+    def fitGD(self,
+              xtrain,
+              ytrain,
+              xvalid=None,
+              yvalid=None,
+              num_epochs=10,
+              batch_size=32,
+              ret_train_loss=False,
+              learn_rate=0.01,
+              optim=gradientDescent()):
+        # the fit method is basically the same as the neural net base, other than the transformation of the features
+        # that needs to take place before fitting
         xtrain = self._getPolynomialFeatures(xtrain)
         if xvalid is not None:
             xvalid = self._getPolynomialFeatures(xvalid)
         # Number of features is on the rows, so num_input == len(X_poly)
         self.num_input = len(xtrain)
-        # Linear regression models have one layer with one neuron using an identity activation function 
+        # Linear regression models have one layer with one neuron using an identity activation function
         activ = IdentityActivation()
         self.add_layer(1, activ)
-        # If ret_train_loss is true, we will return a list of the losses averaged over each epoch for the training set and 
-        # the validation set 
-        
-        return self.fit(xtrain, ytrain, xvalid = xvalid, yvalid = yvalid, num_epochs = num_epochs, batch_size = batch_size, ret_train_loss = ret_train_loss, learn_rate = learn_rate, optim = optim)
+        # If ret_train_loss is true, we will return a list of the losses averaged over each epoch for the training set and
+        # the validation set
+
+        return self.fit(xtrain,
+                        ytrain,
+                        xvalid=xvalid,
+                        yvalid=yvalid,
+                        num_epochs=num_epochs,
+                        batch_size=batch_size,
+                        ret_train_loss=ret_train_loss,
+                        learn_rate=learn_rate,
+                        optim=optim)
 
     def predictLR(self, X):
-        # the predict method is basically the same as the neural net base, other than the transformation of the features 
-        # to the polynomial features that needs to take place before fitting 
+        # the predict method is basically the same as the neural net base, other than the transformation of the features
+        # to the polynomial features that needs to take place before fitting
         X_poly = self._getPolynomialFeatures(X)
         return self.predict(X_poly)
 
@@ -59,27 +75,41 @@ class Base_Regression(NeuralNetwork_Base):
         # Using the combo of the features which is a tuple like (0,0,0)
         # We can say np.prod(dataset[combo_features,:], axis=0) which will do
         # an element by element multiplication along the feature values in
-        # each corresponding column 
+        # each corresponding column
         for feature_idx, combo_features in enumerate(allCombos):
-            new_X[feature_idx, :] = np.prod(dataset[combo_features, :], axis = 0)
-        # Preprocess your new dataset after you've made all the features that you want to use 
+            new_X[feature_idx, :] = np.prod(dataset[combo_features, :], axis=0)
+        # Preprocess your new dataset after you've made all the features that you want to use
         new_X = preprocessing.scale(new_X.T)
         return new_X.T
 
     def _getCombos(self, numFeatures, degree):
-        allCombos = [combinations_with_replacement(range(numFeatures),i) for i in range(1, degree+1)]
+        allCombos = [
+            combinations_with_replacement(range(numFeatures), i)
+            for i in range(1, degree + 1)
+        ]
         unrolled = [item for sublist in allCombos for item in sublist]
         return unrolled
 
+
 class LinearRegression(Base_Regression):
+
     def __init__(self, degree):
-        super(LinearRegression, self).__init__(degree = degree, regularization= None, regParameter=None)
+        super(LinearRegression, self).__init__(degree=degree,
+                                               regularization=None,
+                                               regParameter=None)
+
 
 class RidgeRegression(Base_Regression):
-    def __init__(self, degree, regParam = 0.2):
-        super(RidgeRegression, self).__init__(degree = degree, regularization= "L2", regParameter=regParam)
+
+    def __init__(self, degree, regParam=0.2):
+        super(RidgeRegression, self).__init__(degree=degree,
+                                              regularization="L2",
+                                              regParameter=regParam)
+
 
 class LassoRegression(Base_Regression):
-    def __init__(self, degree, regParam = 0.2):
-        super(LassoRegression, self).__init__(degree = degree, regularization= "L1", regParameter=regParam)
 
+    def __init__(self, degree, regParam=0.2):
+        super(LassoRegression, self).__init__(degree=degree,
+                                              regularization="L1",
+                                              regParameter=regParam)
