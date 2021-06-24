ecad137f06928e752f15b9767307fa67fe856b4d
13jk59 <jesse.khaira10@gmail.com>
2021-06-24 17:36:11 -0600

    updated module

--- ML_algorithms/tests/unsupervised_learning/testPCA.py
+++ ML_algorithms/tests/unsupervised_learning/testPCA.py
@@ -1,24 +1,23 @@
-import tensorflow as tf 
-import numpy as np 
+import tensorflow as tf
+import numpy as np
 from ML_algorithms.unsupervised_learning.PCA import PrincipalComponentAnalysis
 import unittest
-from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
 (x_train, _), (_, _) = tf.keras.datasets.mnist.load_data()
-x_train = np.array(x_train, dtype = float)
+x_train = np.array(x_train, dtype=float)
 x_train /= 255
 
-x_trainOwn = x_train.reshape(-1,60000)
+x_trainOwn = x_train.reshape(-1, 60000)
+
 
 class tests(unittest.TestCase):
+
     def test1(self):
-        # looks fine 
+        # looks fine
         pca = PrincipalComponentAnalysis()
-        transformed_X = pca.fit_transform(x_trainOwn,10)
-        plt.scatter(transformed_X[0,:200], transformed_X[1,:200])s
-
-
+        transformed_X = pca.fit_transform(x_trainOwn, 10)
+        plt.scatter(transformed_X[0, :200], transformed_X[1, :200])
 
 
 if __name__ == "__main__":
