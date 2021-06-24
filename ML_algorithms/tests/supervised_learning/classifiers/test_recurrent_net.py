5709c382cc661d8de3df0f58242c97c5f9e8ce09
13jk59 <jesse.khaira10@gmail.com>
2021-06-24 15:44:38 -0600

    cleaned up rnn test module

--- ML_algorithms/tests/supervised_learning/classifiers/test_recurrent_net.py
+++ ML_algorithms/tests/supervised_learning/classifiers/test_recurrent_net.py
@@ -7,36 +7,41 @@ import unittest
 import sys
 import os
 
-data = open(os.path.join(sys.path[0], "shake.txt"), "r").read()
-chars = getUniqueChars(data)
-idxCharMap = mapidxToChar(chars)
-charIdxMap = mapcharToIdx(chars)
-
 
 class TestRecurrentNetwork(unittest.TestCase):
     """ This class contains unit tests for the recurrent neural network defined
     in the package """
 
+    def setUp(self):
+        self.data = open(os.path.join(sys.path[0], "shake.txt"), "r").read()
+        self.chars = getUniqueChars(self.data)
+        self.idx_char_map = mapidxToChar(self.chars)
+        self.char_idx_map = mapcharToIdx(self.chars)
+
     def testOverfitSmallBatch(self):
         # this makes sure the forward pass and the backward pass are
         # working correctly before doing expensive optimization
-        rnn = ReccurentNetLanguageModel(idxCharMap,
-                                        charIdxMap,
+        rnn = ReccurentNetLanguageModel(self.idx_char_map,
+                                        self.char_idx_map,
                                         TanH(),
                                         550,
-                                        len(chars),
+                                        len(self.chars),
                                         temperature=0.3)
-        rnn.fit(data[:100], 25, verbose=True, num_epochs=5000, learn_rate=0.1)
+        rnn.fit(self.data[:100],
+                25,
+                verbose=True,
+                num_epochs=5000,
+                learn_rate=0.1)
 
     def testFitData(self):
         # takes a long time to fit but achieves okay performance
-        rnn = ReccurentNetLanguageModel(idxCharMap,
-                                        charIdxMap,
+        rnn = ReccurentNetLanguageModel(self.idx_char_map,
+                                        self.char_idx_map,
                                         TanH(),
                                         550,
-                                        len(chars),
+                                        len(self.chars),
                                         temperature=0.3)
-        rnn.fit(data, 25, verbose=True, num_epochs=5000, learn_rate=0.1)
+        rnn.fit(self.data, 25, verbose=True, num_epochs=5000, learn_rate=0.1)
 
 
 if __name__ == "__main__":
