8826c4122308049cbd693c8e4e5e3844b3194925
13jk59 <jesse.khaira10@gmail.com>
2021-07-04 14:23:43 -0600

    cleaned up documnetation

--- machine_learning_algorithms/supervised_learning/classifiers/k_nearest_neighbours_classifier.py
+++ machine_learning_algorithms/supervised_learning/classifiers/k_nearest_neighbours_classifier.py
@@ -5,20 +5,23 @@ from machine_learning_algorithms.supervised_learning.base_classes.k_nearest_neig
 
 
 class KNearestNeighboursClassifier(KNearestNeighboursBase):
-    """
-    This is a minimal implementation of the nonparametric supervised
-    machine learning algorithm called kNN, used for classification.
+    """ This is an implementation of the nonparametric supervised
+    machine learning algorithm called k nearest neighbours, used for
+    classification.
 
-    Params:
+    Args:
+        k:
+            Number of neighbours to consider when making a prediction.
+            Default to consider is 10.
 
-    k -> Number of neighbours to consider when making a prediction.
-    Default to consider is 10.
+        similarity_metric:
+            Metric to use to determine the similarity between
+            different vectors. Default is euclidean distance
+            (L2 distance).
 
-    similarity_metric -> Metric to use to determine the similarity between
-    different vectors. Default is euclidean distance (L2 distance).
-
-    verbose -> Boolean value that determines whether or not to provide updates
-    when the model is predicting on new examples.
+        verbose:
+            Boolean value that determines whether or not to provide updates
+            when the model is predicting on new examples.
     """
 
     def __init__(self, k=10, similarity_metric="L2", verbose=True):
@@ -27,14 +30,16 @@ class KNearestNeighboursClassifier(KNearestNeighboursBase):
               self).__init__(k, similarity_metric, verbose)
 
     def _get_prediction(self, k_closest):
-        """
-        This method returns an integer describing the label of the current vector.
+        """ This method returns an integer describing the label of the
+        current vector.
 
-        Input:
-        k_closest (NumPy matrix) -> Matrix of shape (K, 2) containing the K closest vectors
-        with their corresponding labels 
+        Args:
+            k_closest:
+                Numpy array of shape (K, 2) containing the K closest vectors
+                with their corresponding labels
 
-        Output (int) -> label of the vector 
+        Returns:
+            An integer representing the label of the vector 
         """
         unique, counts = np.unique(k_closest, return_counts=True)
         return unique[np.argmax(counts)]
