a216275f655350eb6de0ca5c0c292836e4f81f57
13jk59 <jesse.khaira10@gmail.com>
2021-08-06 10:11:09 -0600

    cleaned up docstring

--- machine_learning_algorithms/supervised_learning/classifiers/softmax_regression.py
+++ machine_learning_algorithms/supervised_learning/classifiers/softmax_regression.py
@@ -4,16 +4,25 @@ from machine_learning_algorithms.neural_net_utility.activation_functions import 
 
 
 class SoftmaxRegression(NeuralNetworkBase):
-    """
-    This class is a template you can use to create softmax regresssion objects. 
-    The softmax regression object is used for multiclass classification tasks, where
-    the output labels are one-hot encoded to be of shape (C,M). The object contains 
-    one hidden layer with numClasses neurons using the softmax activation function.
+    """ This class represents the softmax regression
+    algorithm used for the task of multi class
+    classification, where the output labels are one-hot
+    encoded to be of shape (C,M). The object contains
+    one hidden layer with numClasses neurons using the
+    softmax activation function.
 
-    Parameters:
-    -> inLayerNeuron (int): Integer representing how many features are at the input to the classifier
-    -> regularization (str): Type of regularization to use. Either "L2" or "L1" is accepted.
-    -> regParameter(int): Integer representing the strength of the regularization
+    Attributes:
+        inLayerNeuron:
+            Integer representing how many features are at the
+            input to the classifier
+
+        regularization:
+            Value that is either a string type that is either "L2" or "L1"
+            or a value of None type, representing the type of regularization
+            to use
+
+        regParameter:
+            Floating point value representing the strength of the regularization
     """
 
     def __init__(self,
