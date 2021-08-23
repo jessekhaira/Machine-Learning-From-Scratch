813937b723e8d16910eca2c22e1408f872b1fb28
13jk59 <jesse.khaira10@gmail.com>
2021-08-22 21:24:37 -0600

    lots of cleaning, type annotating, documenting

--- machine_learning_algorithms/unsupervised_learning/k_means.py
+++ machine_learning_algorithms/unsupervised_learning/k_means.py
@@ -1,13 +1,14 @@
+""" This module contains code representing the unsupervised
+machine learning algorithm K-Means """
 import numpy as np
 from machine_learning_algorithms.utility.misc import euclideanDistance
 import copy
 
 
-class KMeansClustering(object):
-    """
-    This class represents the K-Means algorithm, which is an
-    unsupervised machine learning algorithm used to find hidden structure
-    inside of data.
+class KMeansClustering:
+    """ This class represents the K-Means algorithm. The K-Means algorithm
+    is an unsupervised machine learning algorithm used to find hidden
+    structure inside of data.
 
     Attributes:
         maxNumLoops:
@@ -23,18 +24,18 @@ class KMeansClustering(object):
             centroid every vector in the input data will have
     """
 
-    def __init__(self, maxNumLoops=300):
+    def __init__(self, maxNumLoops: int = 300):
         # need a container to hold each cluster centroid
-        self.clusterCentroids = None
+        self.cluster_centroids = None
         # need a container to hold the vectors each cluster has been assigned to
-        self.clustersVectorsAssigned = None
-        self.maxNumLoops = maxNumLoops
+        self.cluster_vectors_assigned = None
+        self.max_num_loops = maxNumLoops
 
     def fit_predict(self,
-                    data,
-                    num_clusters,
-                    similarityMetric=euclideanDistance,
-                    seed=21):
+                    data: np.ndarray,
+                    num_clusters: int,
+                    similarity_metric=euclideanDistance,
+                    seed: int = 21):
         """ This method computes cluster centroids for every vector in the data
         and returns them.
 
@@ -47,10 +48,10 @@ class KMeansClustering(object):
                 Integer representing the number of clusters the user would
                 like to discover in the data
 
-            similarityMetric:
+            similarity_metric:
                 Function used to compute the similarity between vectors
 
-            seed:  
+            seed:
                 Integer representing the seed to use when randomly generating
                 the cluster centroids
 
@@ -59,55 +60,63 @@ class KMeansClustering(object):
             every vector in the data has been assigned to.
         """
 
-        self._initializeClusterCentroids(num_clusters, data, seed)
-        self.clustersVectorsAssigned = np.zeros((1, data.shape[1]))
+        self._init_cluster_centroids(num_clusters, data, seed)
+        self.cluster_vectors_assigned = np.zeros((1, data.shape[1]))
 
         # we assign vectors to the cluster centroid they are the closest to
         # and then update the cluster centroid until convergence
-        for _ in range(self.maxNumLoops):
+        for _ in range(self.max_num_loops):
             for i in range(data.shape[1]):
                 vector = data[:, i]
-                self._updateCluster(vector, similarityMetric, i)
-            # update cluster centroids to be the avg of the (new) points assigned to it
-            oldClusterCentroids = copy.copy(self.clusterCentroids)
-            self._updateClusterCentroids(data)
+                self._update_cluster(vector, similarity_metric, i)
+            # update cluster centroids to be the avg of the (new)
+            # points assigned to it
+            old_cluster_centroids = copy.copy(self.cluster_centroids)
+            self._update_cluster_centroids(data)
             # CONVERGENCE check - did cluster centroids change at all
-            didCentroidsChange = oldClusterCentroids - self.clusterCentroids
-            # if all values are zero, then that means no cluster centroids changed
-            # and we're done
-            if not didCentroidsChange.any():
+            did_centroids_change = (old_cluster_centroids -
+                                    self.cluster_centroids)
+            # if all values are zero, then that means no cluster
+            # centroids changed and we're done
+            if not did_centroids_change.any():
                 break
-        return self.clustersVectorsAssigned
+        return self.cluster_vectors_assigned
 
-    def _updateClusterCentroids(self, data):
-        for i in range(self.clusterCentroids.shape[1]):
+    def _update_cluster_centroids(self, data: np.ndarray):
+        for i in range(self.cluster_centroids.shape[1]):
             # get all pts assigned to this cluster
-            pts_cluster = np.where(self.clustersVectorsAssigned[:] == i)[1]
+            pts_cluster = np.where(self.cluster_vectors_assigned[:] == i)[1]
             # pull out the vectors from the data
             matrix_cluster = data[:, pts_cluster]
-            # change the cluster centroid to be the average of the pts assigned to this cluster
-            self.clusterCentroids[:, i] = np.mean(matrix_cluster, axis=1)
+            # change the cluster centroid to be the average of the pts
+            # assigned to this cluster
+            self.cluster_centroids[:, i] = np.mean(matrix_cluster, axis=1)
 
-    def _updateCluster(self, vector, similarityMetric, vectorPos):
+    def _update_cluster(self, vector: np.ndarray, similarity_metric,
+                        vector_pos: int) -> None:
         # this method updates the cluster centroid each vector is assigned to
-        minDistance = float('inf')
+        min_distance = float("inf")
         cluster = -1
-        # compare this vector to every single cluster centroid and assign this vector
-        # to the cluster centroid it is the closest to (most similar to)
-        for i in range(self.clusterCentroids.shape[1]):
-            center = self.clusterCentroids[:, i]
-            distance_ithCentroid = similarityMetric(vector, center)
-            if distance_ithCentroid < minDistance:
-                minDistance = distance_ithCentroid
+        # compare this vector to every single cluster centroid and
+        # assign this vector to the cluster centroid it is the
+        # closest to (most similar to)
+        for i in range(self.cluster_centroids.shape[1]):
+            center = self.cluster_centroids[:, i]
+            dist_ith_centroid = similarity_metric(vector, center)
+            if dist_ith_centroid < min_distance:
+                min_distance = dist_ith_centroid
                 cluster = i
 
-        assert cluster != -1, "The cluster this vector is assigned to is still -1, somethings wrong"
-        self.clustersVectorsAssigned[:, vectorPos] = cluster
+        assert cluster != -1, ("The cluster this vector is assigned to " +
+                               "is still -1, somethings wrong")
+        self.cluster_vectors_assigned[:, vector_pos] = cluster
 
-    def _initializeClusterCentroids(self, num_clusters, data, seed):
-        # this method randomly chooses vectors in the data to be cluster centroids
+    def _init_cluster_centroids(self, num_clusters: int, data: np.ndarray,
+                                seed: int) -> None:
+        # this method randomly chooses vectors in the data to be
+        # cluster centroids
         np.random.seed(seed)
-        self.clusterCentroids = np.zeros((data.shape[0], num_clusters))
+        self.cluster_centroids = np.zeros((data.shape[0], num_clusters))
         for i in range(num_clusters):
             idx_vector = np.random.choice(data.shape[1])
-            self.clusterCentroids[:, i] = data[:, idx_vector]
+            self.cluster_centroids[:, i] = data[:, idx_vector]
