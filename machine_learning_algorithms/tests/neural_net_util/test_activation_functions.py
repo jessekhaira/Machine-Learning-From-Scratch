b91802d8bc58e997d152dd01ce91afa8f512b933
13jk59 <jesse.khaira10@gmail.com>
2021-08-28 16:28:36 -0600

    more cleaning

--- machine_learning_algorithms/tests/neural_net_util/test_activation_functions.py
+++ machine_learning_algorithms/tests/neural_net_util/test_activation_functions.py
@@ -1,3 +1,5 @@
+""" This module contains unit tests for a variety of
+activation functions """
 import numpy as np
 import unittest
 from machine_learning_algorithms.neural_net_utility.activation_functions import Softmax
@@ -11,21 +13,23 @@ class TestActivationFunctions(unittest.TestCase):
     def test1(self):
         #test with single example first
         np.random.seed(21)
-        sm = Softmax()
+        softmax_predictor = Softmax()
         ce = cross_entropy()
-        Z1 = np.random.randn(4, 5)
-        A1 = sm.compute_output(Z1)
-        print(A1)
-        Y = np.array([[0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1], [1, 0, 0, 0],
+        z = np.random.randn(4, 5)
+        a = softmax_predictor.compute_output(z)
+        print(a)
+        y = np.array([[0, 0, 1, 0], [0, 1, 0, 0], [0, 0, 0, 1], [1, 0, 0, 0],
                       [0, 1, 0, 0]]).T
-        print(ce.get_loss(Y, A1))
-        print(Y)
-        dLdA1 = ce.get_gradient_pred(Y, A1)
-        print(dLdA1)
-        dLdZ = dl_dz_softmax(Z1, A1, dLdA1, sm)
-        # Only one value in each column should be negative, the rest should be positive
-        # and the values should be easily correlated with (a-1)/m (if y =1) else (a)/m
-        print(dLdZ)
+        print(ce.get_loss(y, a))
+        print(y)
+        dl_da = ce.get_gradient_pred(y, a)
+        print(dl_da)
+        dl_dz = dl_dz_softmax(z, a, dl_da, softmax_predictor)
+        # Only one value in each column should be negative,
+        # the rest should be positive and the values should
+        # be easily correlated with (a-1)/m (if y =1) else
+        # (a)/m
+        print(dl_dz)
 
 
 if __name__ == "__main__":
