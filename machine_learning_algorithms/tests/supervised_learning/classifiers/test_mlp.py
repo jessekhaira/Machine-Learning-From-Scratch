bb6d1a78c1694d8ca43a8223a7c46ecb83521426
13jk59 <jesse.khaira10@gmail.com>
2021-10-31 13:54:41 -0600

    lots of work on testing module

--- machine_learning_algorithms/tests/supervised_learning/classifiers/test_mlp.py
+++ machine_learning_algorithms/tests/supervised_learning/classifiers/test_mlp.py
@@ -12,55 +12,58 @@ from machine_learning_algorithms.supervised_learning.classifiers.softmax_regress
 from machine_learning_algorithms.neural_net_utility.activation_functions import ReLU, TanH, Sigmoid, Softmax
 
 
+def create_spiral_dataset():
+    # Spiral Dataset from CS231N
+    N = 100    # number of points per class
+    D = 2    # dimensionality
+    K = 3    # number of classes
+    x = np.zeros((N * K, D))    # data matrix (each row = single example)
+    y = np.zeros(N * K, dtype="uint8")    # class labels
+    for j in range(K):
+        ix = range(N * j, N * (j + 1))
+        r = np.linspace(0.0, 1, N)    # radius
+        t = np.linspace(j * 4,
+                        (j + 1) * 4, N) + np.random.randn(N) * 0.2    # theta
+        x[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
+        y[ix] = j
+
+    x = x.T
+    notEncodedy = y
+    y = oneHotEncode(y.reshape(1, -1))
+    return x, y, notEncodedy
+
+
 class TestMultiLayerPerceptron(unittest.TestCase):
 
     def setUp(self) -> None:
-        X, Y = load_breast_cancer(return_X_y=True)
-        X_train, X_test, y_train, y_test = train_test_split(X,
-                                                            Y,
-                                                            test_size=0.15,
-                                                            random_state=42)
-        X_train = preprocessing.scale(X_train)
-        X_test = preprocessing.scale(X_test).T
-        y_test = y_test.T.reshape(1, -1)
+        self.x, self.y = load_breast_cancer(return_X_y=True)
+        self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
+            self.x, self.y, test_size=0.15, random_state=42)
+        self.x_train = preprocessing.scale(self.x_train)
+        self.x_test = preprocessing.scale(self.x_test).T
+        self.y_test = self.y_test.T.reshape(1, -1)
 
-        X_train, X_valid, y_train, y_valid = train_test_split(X_train,
-                                                              y_train,
-                                                              test_size=0.15,
-                                                              random_state=42)
+        self.x_train, self.x_valid, self.y_train, self.y_valid = (
+            train_test_split(self.x_train,
+                             self.y_train,
+                             test_size=0.15,
+                             random_state=42))
 
-        X_train = X_train.T
-        X_valid = X_valid.T
-        y_train = y_train.T.reshape(1, -1)
-        y_valid = y_valid.T.reshape(1, -1)
-
-        # Spiral Dataset from CS231N
-        N = 100    # number of points per class
-        D = 2    # dimensionality
-        K = 3    # number of classes
-        X = np.zeros((N * K, D))    # data matrix (each row = single example)
-        y = np.zeros(N * K, dtype='uint8')    # class labels
-        for j in range(K):
-            ix = range(N * j, N * (j + 1))
-            r = np.linspace(0.0, 1, N)    # radius
-            t = np.linspace(j * 4, (j + 1) * 4,
-                            N) + np.random.randn(N) * 0.2    # theta
-            X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
-            y[ix] = j
-
-        X = X.T
-        notEncodedy = y
-        y = oneHotEncode(y.reshape(1, -1))
+        self.x_train = self.x_train.T
+        self.x_valid = self.x_valid.T
+        self.y_train = self.y_train.T.reshape(1, -1)
+        self.y_valid = self.y_valid.T.reshape(1, -1)
 
         return super().setUp()
 
-    def testBinaryUnregularized(self):
-        # Learning rate KEY: If its set to high, you will diverge. If its set to low,
-        # your network won't learn anything. The more layers you have, the higher the learning rate
-        # should be. Example seen here: 1 layer only - learn rate 0.1 suffices. With two layers ~ 0.7.
-        # with 3 layers ~2.5.
+    def test_binaryclassification_unregularized(self):
+        """ Learning rate KEY: If its set to high, you will diverge. If its set
+        to low, your network won"t learn anything. The more layers you have,
+        the higher the learning rate should be. Example seen here:
+        1 layer only - learn rate 0.1 suffices. With
+        two layers ~ 0.7, with 3 layers ~2.5. """
         MLP = MultiLayerPerceptron(typeSupervised="binary",
-                                   numberInputFeatures=X_train.shape[0])
+                                   numberInputFeatures=self.x_train.shape[0])
         # Just add Dense Layers
         # 10 features learnt in first hidden layer w/ ReLU
         MLP.add_layer(10, activation_function=ReLU())
@@ -70,18 +73,18 @@ class TestMultiLayerPerceptron(unittest.TestCase):
         MLP.add_layer(1, activation_function=Sigmoid())
 
         train_loss, valid_loss, train_acc, valid_acc = MLP.fit(
-            X_train,
-            y_train,
-            X_valid,
-            y_valid,
+            self.x_train,
+            self.y_train,
+            self.x_valid,
+            self.y_valid,
             ret_train_loss=True,
             num_epochs=100,
             learn_rate=2.6)
-        preds = MLP.predict_multi_layer_perceptron(X_test, 0.5)
-        acc = accuracy(y_test, preds)
+        preds = MLP.predict_multi_layer_perceptron(self.x_test, 0.5)
+        acc = accuracy(self.y_test, preds)
         print(acc)
         print(train_loss)
-        print('\n')
+        print("\n")
         print(valid_loss)
 
         # Can get 99.75% w/ a normal logistic regresssion model
@@ -90,11 +93,11 @@ class TestMultiLayerPerceptron(unittest.TestCase):
     def testHighRegBinary(self):
         # Sanity check - high regularization leads to very high losses.
         MLP1 = MultiLayerPerceptron(typeSupervised="binary",
-                                    numberInputFeatures=X_train.shape[0],
+                                    numberInputFeatures=self.x_train.shape[0],
                                     regularization="L1",
                                     reg_parameter=500)
         MLP2 = MultiLayerPerceptron(typeSupervised="binary",
-                                    numberInputFeatures=X_train.shape[0],
+                                    numberInputFeatures=self.x_train.shape[0],
                                     regularization="L2",
                                     reg_parameter=500)
         # 10 features learnt in first hidden layer w/ ReLU
@@ -112,49 +115,49 @@ class TestMultiLayerPerceptron(unittest.TestCase):
         MLP2.add_layer(1, activation_function=Sigmoid())
 
         train_loss1, valid_loss1, train_acc1, valid_acc1 = MLP1.fit(
-            X_train,
-            y_train,
-            X_valid,
-            y_valid,
+            self.x_train,
+            self.y_train,
+            self.x_valid,
+            self.y_valid,
             ret_train_loss=True,
             num_epochs=10,
             learn_rate=2.6)
-        preds1 = MLP1.predict_multi_layer_perceptron(X_test, 0.5)
-        acc1 = accuracy(y_test, preds1)
+        preds1 = MLP1.predict_multi_layer_perceptron(self.x_test, 0.5)
+        acc1 = accuracy(self.y_test, preds1)
         print(acc1)
         print(train_loss1)
-        print('\n')
+        print("\n")
         print(valid_loss1)
 
         train_loss2, valid_loss2, train_acc2, valid_acc2 = MLP2.fit(
-            X_train,
-            y_train,
-            X_valid,
-            y_valid,
+            self.x_train,
+            self.y_train,
+            self.x_valid,
+            self.y_valid,
             ret_train_loss=True,
             num_epochs=10,
             learn_rate=3)
-        preds2 = MLP2.predict_multi_layer_perceptron(X_test, 0.5)
-        acc2 = accuracy(y_test, preds2)
+        preds2 = MLP2.predict_multi_layer_perceptron(self.x_test, 0.5)
+        acc2 = accuracy(self.y_test, preds2)
         print(acc2)
         print(train_loss2)
-        print('\n')
+        print("\n")
         print(valid_loss2)
 
     def testNormalRegBinary(self):
-        """
-        You have to be really careful with the reg parameter. If its to high (past 0.1), your network
-        won't learn anything. With softmax regression and logistic regression, there existed leeway
-        to make the reg parameter pretty high but that absolutely won't work with neural networks multiple
+        """ You have to be really careful with the reg parameter. If its to high
+        (past 0.1), your network won't learn anything. With softmax regression
+        and logistic regression, there existed leeway to make the reg parameter
+        pretty high but that absolutely won"t work with neural networks multiple
         layers deep.
         """
         # normal regularization
         MLP3 = MultiLayerPerceptron(typeSupervised="binary",
-                                    numberInputFeatures=X_train.shape[0],
+                                    numberInputFeatures=self.x_train.shape[0],
                                     regularization="L1",
                                     reg_parameter=0.01)
         MLP4 = MultiLayerPerceptron(typeSupervised="binary",
-                                    numberInputFeatures=X_train.shape[0],
+                                    numberInputFeatures=self.x_train.shape[0],
                                     regularization="L2",
                                     reg_parameter=0.01)
 
@@ -173,48 +176,49 @@ class TestMultiLayerPerceptron(unittest.TestCase):
         MLP4.add_layer(1, activation_function=Sigmoid())
 
         train_loss3, valid_loss3, train_acc3, valid_acc3 = MLP3.fit(
-            X_train,
-            y_train,
-            X_valid,
-            y_valid,
+            self.x_train,
+            self.y_train,
+            self.x_valid,
+            self.y_valid,
             ret_train_loss=True,
             num_epochs=100,
             learn_rate=2.8)
-        preds3 = MLP3.predict_multi_layer_perceptron(X_test, 0.5)
-        acc3 = accuracy(y_test, preds3)
+        preds3 = MLP3.predict_multi_layer_perceptron(self.x_test, 0.5)
+        acc3 = accuracy(self.y_test, preds3)
         print(acc3)
-        print('\n')
+        print("\n")
         print(train_loss3)
-        print('\n')
+        print("\n")
         print(valid_loss3)
-        print('\n')
+        print("\n")
 
         train_loss4, valid_loss4, train_acc4, valid_acc4 = MLP4.fit(
-            X_train,
-            y_train,
-            X_valid,
-            y_valid,
+            self.x_train,
+            self.y_train,
+            self.x_valid,
+            self.y_valid,
             ret_train_loss=True,
             num_epochs=100,
             learn_rate=2.6)
-        preds4 = MLP4.predict_multi_layer_perceptron(X_test, 0.5)
-        acc4 = accuracy(y_test, preds4)
+        preds4 = MLP4.predict_multi_layer_perceptron(self.x_test, 0.5)
+        acc4 = accuracy(self.y_test, preds4)
         print(acc4)
         print(train_loss4)
-        print('\n')
+        print("\n")
         print(valid_loss4)
-        print('\n')
+        print("\n")
 
     def testMultiClass(self):
         # replicating results from CS231N on this dataset
+        x, y, notEncodedy = create_spiral_dataset()
         sm = SoftmaxRegression(inLayerNeuron=2, numClasses=3)
-        train_loss_sm, train_acc_sm = sm.fit(X,
+        train_loss_sm, train_acc_sm = sm.fit(x,
                                              y,
                                              ret_train_loss=True,
                                              num_epochs=190,
                                              learn_rate=1)
         print(train_loss_sm)
-        preds_sm = sm.predict(X)
+        preds_sm = sm.predict(x)
         acc_sm = accuracy(notEncodedy, preds_sm)
         print(acc_sm)
         # Loss obtained from CS231N - ours should hopefully be better!
@@ -227,13 +231,13 @@ class TestMultiLayerPerceptron(unittest.TestCase):
         MLP6.add_layer(100, activation_function=ReLU())
         # Output layer learn 3 features for softmax
         MLP6.add_layer(3, activation_function=Softmax(), isSoftmax=1)
-        train_loss6, train_acc = MLP6.fit(xtrain=X,
+        train_loss6, train_acc = MLP6.fit(xtrain=x,
                                           ytrain=y,
                                           num_epochs=1000,
                                           learn_rate=1,
                                           ret_train_loss=True)
         print(train_loss6)
-        preds_MLP6 = MLP6.predict_multi_layer_perceptron(X)
+        preds_MLP6 = MLP6.predict_multi_layer_perceptron(x)
         acc_6 = accuracy(notEncodedy, preds_MLP6)
         print(acc_6)
         # Performance without regularization should be above 90%
@@ -249,13 +253,13 @@ class TestMultiLayerPerceptron(unittest.TestCase):
         MLP7.add_layer(100, activation_function=ReLU())
         # Output layer learn 3 features for softmax
         MLP7.add_layer(3, activation_function=Softmax(), isSoftmax=1)
-        train_loss7, train_acc7 = MLP7.fit(xtrain=X,
+        train_loss7, train_acc7 = MLP7.fit(xtrain=x,
                                            ytrain=y,
                                            num_epochs=5000,
                                            learn_rate=1,
                                            ret_train_loss=True)
         print(train_loss7, train_acc7)
-        preds_MLP7 = MLP7.predict_multi_layer_perceptron(X)
+        preds_MLP7 = MLP7.predict_multi_layer_perceptron(x)
         acc_7 = accuracy(notEncodedy, preds_MLP7)
         print(acc_7)
 
