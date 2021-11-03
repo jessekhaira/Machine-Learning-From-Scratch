91cf4d6a4fe5117e2ee3fdfe50143c5dbe705f98
13jk59 <jesse.khaira10@gmail.com>
2021-11-03 14:42:41 -0600

    cleaned up variable names for function

--- machine_learning_algorithms/tests/supervised_learning/classifiers/test_mlp.py
+++ machine_learning_algorithms/tests/supervised_learning/classifiers/test_mlp.py
@@ -14,23 +14,25 @@ from machine_learning_algorithms.neural_net_utility.activation_functions import 
 
 def create_spiral_dataset():
     # Spiral Dataset from CS231N
-    N = 100    # number of points per class
-    D = 2    # dimensionality
-    K = 3    # number of classes
-    x = np.zeros((N * K, D))    # data matrix (each row = single example)
-    y = np.zeros(N * K, dtype="uint8")    # class labels
-    for j in range(K):
-        ix = range(N * j, N * (j + 1))
-        r = np.linspace(0.0, 1, N)    # radius
-        t = np.linspace(j * 4,
-                        (j + 1) * 4, N) + np.random.randn(N) * 0.2    # theta
+    num_points = 100    # number of points per class
+    dimensions = 2    # dimensionality
+    num_classes = 3    # number of classes
+    x = np.zeros((num_points * num_classes,
+                  dimensions))    # data matrix (each row = single example)
+    y = np.zeros(num_points * num_classes, dtype="uint8")    # class labels
+    for j in range(num_classes):
+        ix = range(num_points * j, num_points * (j + 1))
+        r = np.linspace(0.0, 1, num_points)    # radius
+        t = np.linspace(
+            j * 4, (j + 1) * 4,
+            num_points) + np.random.randn(num_points) * 0.2    # theta
         x[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
         y[ix] = j
 
     x = x.T
-    notEncodedy = y
+    not_encoded_y = y
     y = oneHotEncode(y.reshape(1, -1))
-    return x, y, notEncodedy
+    return x, y, not_encoded_y
 
 
 class TestMultiLayerPerceptron(unittest.TestCase):
