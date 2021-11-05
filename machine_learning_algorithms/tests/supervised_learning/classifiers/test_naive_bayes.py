139b2746e78fbff761ed69a26f1bf261928530e4
13jk59 <jesse.khaira10@gmail.com>
2021-11-05 13:56:58 -0600

    added setUp to class

--- machine_learning_algorithms/tests/supervised_learning/classifiers/test_naive_bayes.py
+++ machine_learning_algorithms/tests/supervised_learning/classifiers/test_naive_bayes.py
@@ -7,29 +7,34 @@ from machine_learning_algorithms.supervised_learning.classifiers.gaussian_naive_
 from machine_learning_algorithms.utility.ScoreFunctions import accuracy
 from machine_learning_algorithms.utility.k_Fold_CV import k_fold_CV
 
-X, Y = load_iris(return_X_y=True)
-
-X = preprocessing.scale(X).T
-Y = Y.reshape(1, -1)
-
-X2, Y2 = load_wine(return_X_y=True)
-X2 = X2.T
-Y2 = Y2.reshape(1, -1)
-kObj = k_fold_CV()
-naiveBayes = GaussianNaiveBayes()
-
 
 class TestNaiveBayes(unittest.TestCase):
     """ This class contains unit tests for the naive bayes algorithm"""
 
+    def setUp(self) -> None:
+        self.x, self.y = load_iris(return_X_y=True)
+
+        self.x = preprocessing.scale(self.x).T
+        self.y = self.y.reshape(1, -1)
+
+        self.x1, self.y1 = load_wine(return_X_y=True)
+        self.x1 = self.x1.T
+        self.y1 = self.y1.reshape(1, -1)
+        self.k_obj = k_fold_CV()
+        self.naive_bayes = GaussianNaiveBayes()
+
+        return super().setUp()
+
     def test1(self):
         # achieves a k-fold validated score of 96.6% -> very good.
-        kScore = kObj.getKScore(X, Y, accuracy, naiveBayes)
+        kScore = self.k_obj.getKScore(self.x, self.y, accuracy,
+                                      self.naive_bayes)
         print(kScore)
 
     def test2(self):
         # achieves a k-fold validated score of 100% -> very good
-        kScore = kObj.getKScore(X2, Y2, accuracy, naiveBayes)
+        kScore = self.k_obj.getKScore(self.x1, self.y1, accuracy,
+                                      self.naive_bayes)
         print(kScore)
 
 
