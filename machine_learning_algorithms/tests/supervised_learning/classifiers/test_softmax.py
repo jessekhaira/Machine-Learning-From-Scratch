3507d6773b437fb0d67966326fb8591aa793aa51
13jk59 <jesse.khaira10@gmail.com>
2021-11-08 17:14:16 -0700

    fixed import order

--- machine_learning_algorithms/tests/supervised_learning/classifiers/test_softmax.py
+++ machine_learning_algorithms/tests/supervised_learning/classifiers/test_softmax.py
@@ -1,4 +1,6 @@
 """ This module contains tests for the softmax regression algorithm """
+import unittest
+import numpy as np
 from machine_learning_algorithms.supervised_learning.classifiers.softmax_regression import SoftmaxRegression
 from machine_learning_algorithms.neural_net_utility.optimizer import Adam, RMSProp
 from machine_learning_algorithms.utility.ScoreFunctions import accuracy
@@ -6,8 +8,6 @@ from machine_learning_algorithms.utility.misc import one_hot_encode
 from machine_learning_algorithms.utility.k_Fold_CV import k_fold_CV
 from sklearn.datasets import load_iris
 from sklearn import preprocessing
-import unittest
-import numpy as np
 
 
 class SoftmaxTests(unittest.TestCase):
