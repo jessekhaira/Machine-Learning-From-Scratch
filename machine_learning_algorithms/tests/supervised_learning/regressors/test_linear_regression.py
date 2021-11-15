aa83b95a9f4ee9381fb6f63a7fd57d2db4deb8d9
13jk59 <jesse.khaira10@gmail.com>
2021-11-15 15:08:19 -0700

    deleted print statements

--- machine_learning_algorithms/tests/supervised_learning/regressors/test_linear_regression.py
+++ machine_learning_algorithms/tests/supervised_learning/regressors/test_linear_regression.py
@@ -109,7 +109,6 @@ class TestLinearRegression(unittest.TestCase):
         ridge_sk = sklearn.linear_model.Ridge(alpha=1000)
         ridge_sk.fit(self.x_train.T, self.y_train.ravel())
         preds_ridgesk = ridge_sk.predict(self.x_test.T)
-        print(R_squared(self.y_test, preds_ridgesk))
 
         ridge_obj2 = RidgeRegression(degree=1, regParam=1000)
         ridge_obj2.fit_iterative_optimizer(xtrain=self.x_train,
@@ -117,7 +116,6 @@ class TestLinearRegression(unittest.TestCase):
                                            num_epochs=200,
                                            learn_rate=0.1)
         preds_ridge = ridge_obj2.predict_linear_regression(self.x_test)
-        print(R_squared(self.y_test, preds_ridge))
 
     def test7(self):
         degree_2 = LinearRegression(degree=2)
