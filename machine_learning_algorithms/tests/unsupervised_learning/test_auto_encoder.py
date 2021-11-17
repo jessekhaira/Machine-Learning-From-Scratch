effcd6e7ffd7b2746b11b3ee8df3eff74c373191
13jk59 <jesse.khaira10@gmail.com>
2021-11-16 19:01:27 -0700

    added setUp and appropriate reference

--- machine_learning_algorithms/tests/unsupervised_learning/test_auto_encoder.py
+++ machine_learning_algorithms/tests/unsupervised_learning/test_auto_encoder.py
@@ -6,19 +6,21 @@ import tensorflow as tf
 from machine_learning_algorithms.unsupervised_learning.auto_encoder import AutoEncoder
 from machine_learning_algorithms.unsupervised_learning.auto_encoder import GradientDescentMomentum
 
-(x_train, _), (_, _) = tf.keras.datasets.mnist.load_data()
-x_train = np.array(x_train, dtype=float)
-x_train /= 255
-x_train.reshape(784, -1)
-x_train = x_train.reshape(784, -1)
-
 
 class AutoEncoderTests(unittest.TestCase):
+
+    def setUp(self) -> None:
+        (self.x_train, _), (_, _) = tf.keras.datasets.mnist.load_data()
+        self.x_train = np.array(self.x_train, dtype=float)
+        self.x_train /= 255
+        self.x_train.reshape(784, -1)
+        self.x_train = self.x_train.reshape(784, -1)
+        return super().setUp()
 
     def test1(self):
         # works fine, but takes a really long time to train
         autoencoderObj = AutoEncoder(size_encoding=150)
-        autoencoderObj.fit(x_train,
+        autoencoderObj.fit(self.x_train,
                            num_epochs=10000,
                            optim=GradientDescentMomentum(),
                            ret_train_loss=True,
