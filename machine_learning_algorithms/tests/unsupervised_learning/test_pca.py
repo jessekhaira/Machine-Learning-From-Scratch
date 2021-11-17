1fad372d687135fb290088d7af02f3a7ee868a29
13jk59 <jesse.khaira10@gmail.com>
2021-11-17 16:47:00 -0700

    wrapped code in setUp for test

--- machine_learning_algorithms/tests/unsupervised_learning/test_pca.py
+++ machine_learning_algorithms/tests/unsupervised_learning/test_pca.py
@@ -5,19 +5,20 @@ from machine_learning_algorithms.unsupervised_learning.principal_component_analy
 import unittest
 import matplotlib.pyplot as plt
 
-(x_train, _), (_, _) = tf.keras.datasets.mnist.load_data()
-x_train = np.array(x_train, dtype=float)
-x_train /= 255
-
-x_trainOwn = x_train.reshape(-1, 60000)
-
 
 class tests(unittest.TestCase):
 
+    def setUp(self) -> None:
+        (x, _), (_, _) = tf.keras.datasets.mnist.load_data()
+        x = np.array(x, dtype=float)
+        x /= 255
+
+        self.x_train = x.reshape(-1, 60000)
+        return super().setUp()
+
     def test1(self):
-        # looks fine
         pca = PrincipalComponentAnalysis()
-        transformed_X = pca.fit_transform(x_trainOwn, 10)
+        transformed_X = pca.fit_transform(self.x_train, 10)
         plt.scatter(transformed_X[0, :200], transformed_X[1, :200])
 
 
