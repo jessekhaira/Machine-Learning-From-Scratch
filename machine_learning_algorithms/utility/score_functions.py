aef9ad6852e2c36fb5ed7ea05d72879b7695788c
13jk59 <jesse.khaira10@gmail.com>
2021-11-24 18:08:51 -0700

    added type annotations

--- machine_learning_algorithms/utility/score_functions.py
+++ machine_learning_algorithms/utility/score_functions.py
@@ -16,7 +16,7 @@ def mean_absolute_error(ylabel: np.ndarray, yhat: np.ndarray) -> float:
     return np.abs(ylabel - yhat).mean()
 
 
-def root_mean_squared_error(ylabel, yhat):
+def root_mean_squared_error(ylabel: np.ndarray, yhat: np.ndarray) -> float:
     output = mean_squared_error(ylabel, yhat)
     return output**0.5
 
